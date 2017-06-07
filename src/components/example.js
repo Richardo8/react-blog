5cb98cc18c3c158dc64162cd9cc6a6fe57a981a9
LS <manunited_22@sina.com>
2017-06-07 14:41:07 +0800

    Mount设置计时器

--- src/components/example.js
+++ src/components/example.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
 class Example extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
+
+    componentDidMount(){
+        this.timerId = setInterval(
+            () => this.tick(),
+            1000
+        )
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerId)
+    }
+
+    tick(){
+        this.setState({
+            date: new Date(),
+        })
+    }
+
     render(){
         return (
-            <h1>Hello, {this.props.name}</h1>
+            <div>
+                <h1>Hello, {this.props.name}</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
         )
     }
 }
