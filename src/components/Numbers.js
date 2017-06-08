bde110cb27794a198df63d70946242370e0f31a8
LS <manunited_22@sina.com>
2017-06-08 16:03:29 +0800

    数组渲染 报错是因为缺少key

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Numbers extends Component {
     render(){
         const numbers = this.props.numbers;
-        const listItem = numbers.map((number) => <li>{number}</li>)
+        const listItem = numbers.map((number) => <li key={number.toString()}>{number}</li>)
         return (
             <ul>
                 {listItem}
