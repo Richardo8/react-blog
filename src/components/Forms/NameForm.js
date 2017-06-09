c5d1829db5d6aeb1a60e8d2cb0d55e2b30fbebf6
LS <manunited_22@sina.com>
2017-06-09 14:47:33 +0800

    select的受控组件

--- src/components/Forms/NameForm.js
+++ src/components/Forms/NameForm.js
@@ -4,19 +4,26 @@ class NameForm extends Component {
     constructor(props){
         super(props);
         this.state = {
-            value: ''
+            value: '',
+            select: 'lime'
         };
 
     }
 
-    handleChange = (e) => {
+    handleChangeInput = (e) => {
         this.setState({
             value: e.target.value.toUpperCase(),
         })
     }
 
+    handleChangeSelect = (e) => {
+        this.setState({
+            select: e.target.value,
+        })
+    }
+
     handleSubmit = (e) => {
-        console.log('A name was submitted: ' + this.state.value);
+        console.log('A name was submitted: ' + this.state.value + ' and ' + this.state.select);
         e.preventDefault();
     }
 
@@ -25,8 +32,15 @@ class NameForm extends Component {
             <form onSubmit={this.handleSubmit}>
                 <label>
                     Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
-                    <textarea value={this.state.value} onChange={this.handleChange} />
+                    <input type="text" value={this.state.value} onChange={this.handleChangeInput}/>
+                    <textarea value={this.state.value} onChange={this.handleChangeInput} />
+                    Pick your favorite La Croix Flavor:
+                    <select value={this.state.select} onChange={this.handleChangeSelect}>
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="mango">Mango</option>
+                    </select>
                 </label>
                 <input type="submit" value="Submit"/>
             </form>
