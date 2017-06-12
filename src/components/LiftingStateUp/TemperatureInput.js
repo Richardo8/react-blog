030ff0ae324a2c42c4168a60dcb7a1c64d4047bf
LS <manunited_22@sina.com>
2017-06-12 14:27:55 +0800

    修改输入温度的组件，将数据推向父组件，数据改变后的setState也改变为父组件提供的方法

--- src/components/LiftingStateUp/TemperatureInput.js
+++ src/components/LiftingStateUp/TemperatureInput.js
@@ -8,15 +8,18 @@ const scaleNames = {
 class TemperatureInput extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            temperature: ''
-        }
+        // this.state = {
+        //     temperature: ''
+        // }
     }
 
     handleChange = (e) => {
-        this.setState({
-            temperature: e.target.value
-        })
+        // before
+        // this.setState({
+        //     temperature: e.target.value
+        // })
+
+        this.props.onTemperatureChange(e.target.value)
     }
 
     render(){
@@ -25,7 +28,7 @@ class TemperatureInput extends Component {
                 <legend>Enter temperature in {scaleNames[this.props.scale]}:</legend>
                 <input
                     type="number"
-                    value={this.state.temperature}
+                    value={this.props.temperature}
                     onChange={this.handleChange} />
                 {/*<BoilingVerdict celsius={parseFloat(this.state.temperature)}/>*/}
             </fieldset>
