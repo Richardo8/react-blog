977a7d6e5dffe3997341ceffda6262478ff95c33
LS <manunited_22@sina.com>
2017-06-15 15:03:46 +0800

    state需要判断什么时候用 放在哪儿

--- src/components/ThingkingInReact/FilterableProductTable.js
+++ src/components/ThingkingInReact/FilterableProductTable.js
@@ -3,11 +3,18 @@ import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
 
 class FilterableProductTable extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            filterText: '',
+            inStockOnly: false
+        }
+    }
     render(){
         return (
             <div>
-                <SearchBar/>
-                <ProductTable products={this.props.products}/>
+                <SearchBar filterText={this.state.filterText} inStockOnly={this.state.inStockOnly}/>
+                <ProductTable products={this.props.products} filterText={this.state.filterText} inStockOnly={this.state.inStockOnly}/>
             </div>
         )
     }
