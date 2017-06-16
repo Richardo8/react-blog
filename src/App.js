e62c2b3ce92e77dd7a1592da08053fea9cca459c
LS <manunited_22@sina.com>
2017-06-16 17:48:07 +0800

    Basic React-Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Example from './components/example';
+import BasicExample from './RouterExample/RouterExample';
 import './App.css';
 
 class App extends Component {
@@ -14,7 +15,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Example name="L" />
+        {/*<Example name="L" />*/}
+        <BasicExample/>
       </div>
     );
   }
