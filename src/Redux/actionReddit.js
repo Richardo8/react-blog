bb517ea93f36e3ff6bcf946c6ae0ddf86a738631
LS <manunited_22@sina.com>
2017-07-06 15:45:16 +0800

    将获取数据路径换回原地址

--- src/Redux/actionReddit.js
+++ src/Redux/actionReddit.js
@@ -36,7 +36,7 @@ function receivePosts(subreddit, json) {
 function fetchPosts(subreddit) {
     return dispatch => {
         dispatch(requestPosts(subreddit))
-        return fetch(`/api/${subreddit}.json`)
+        return fetch(`https://www.reddit.com/r/${subreddit}.json`)
             .then(response => response.json())
             .then(json => dispatch(receivePosts(subreddit, json)))
     }
